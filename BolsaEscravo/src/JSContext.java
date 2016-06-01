60c216c1baba5dbec6cb15f750d6e9d9ba5e3ddb
Diego Wallace Santos <diegows2012@gmail.com>
2016-06-01 19:09:41 -0300

    Atualização do modo de leitura

--- BolsaEscravo/src/JSContext.java
+++ BolsaEscravo/src/JSContext.java
@@ -28,49 +28,50 @@ public class JSContext {
 	    int acertos = 0;
 		ScriptContext newContext = new SimpleScriptContext();
         Bindings engineScope = newContext.getBindings(ScriptContext.ENGINE_SCOPE);
-        
+        String[] valores = new String[25];
         for(int tam = arq.getSize()-5; tam >=3; tam --){
-        	engineScope.put("P1_OPEN" ,Double.parseDouble(arq.lerArquivo("Open", tam)));
-        	engineScope.put("P1_MAX" ,Double.parseDouble(arq.lerArquivo("High", tam)));
-            engineScope.put("P1_MIN" ,Double.parseDouble(arq.lerArquivo("Low", tam)));
-            engineScope.put("P1_CLOSE" ,Double.parseDouble(arq.lerArquivo("Close", tam)));
+        	valores = arq.lerArquivo(tam);
+        	
+        	engineScope.put("P1_OPEN" ,Double.parseDouble(valores[0]));
+        	engineScope.put("P1_MAX" ,Double.parseDouble(valores[1]));
+            engineScope.put("P1_MIN" ,Double.parseDouble(valores[2]));
+            engineScope.put("P1_CLOSE" ,Double.parseDouble(valores[3]));
             
-            engineScope.put("P2_OPEN" ,Double.parseDouble(arq.lerArquivo("Open", tam+1)));
-            engineScope.put("P2_MAX" ,Double.parseDouble(arq.lerArquivo("High", tam+1)));
-            engineScope.put("P2_MIN" ,Double.parseDouble(arq.lerArquivo("Low", tam+1)));
-            engineScope.put("P2_CLOSE" ,Double.parseDouble(arq.lerArquivo("Close", tam+1)));
+            engineScope.put("P2_OPEN" ,Double.parseDouble(valores[4]));
+            engineScope.put("P2_MAX" ,Double.parseDouble(valores[5]));
+            engineScope.put("P2_MIN" ,Double.parseDouble(valores[6]));
+            engineScope.put("P2_CLOSE" ,Double.parseDouble(valores[7]));
             
-            engineScope.put("P3_OPEN" ,Double.parseDouble(arq.lerArquivo("Open", tam+2)));
-            engineScope.put("P3_MAX" ,Double.parseDouble(arq.lerArquivo("High", tam+2)));
-            engineScope.put("P3_MIN" ,Double.parseDouble(arq.lerArquivo("Low", tam+2)));
-            engineScope.put("P3_CLOSE" ,Double.parseDouble(arq.lerArquivo("Close", tam+2)));
+            engineScope.put("P3_OPEN" ,Double.parseDouble(valores[8]));
+            engineScope.put("P3_MAX" ,Double.parseDouble(valores[9]));
+            engineScope.put("P3_MIN" ,Double.parseDouble(valores[10]));
+            engineScope.put("P3_CLOSE" ,Double.parseDouble(valores[11]));
             
-            engineScope.put("P4_OPEN" ,Double.parseDouble(arq.lerArquivo("Open", tam+3)));
-            engineScope.put("P4_MAX" ,Double.parseDouble(arq.lerArquivo("High", tam+3)));
-            engineScope.put("P4_MIN" ,Double.parseDouble(arq.lerArquivo("Low", tam+3)));
-            engineScope.put("P4_CLOSE" ,Double.parseDouble(arq.lerArquivo("Close", tam+3)));
+            engineScope.put("P4_OPEN" ,Double.parseDouble(valores[12]));
+            engineScope.put("P4_MAX" ,Double.parseDouble(valores[13]));
+            engineScope.put("P4_MIN" ,Double.parseDouble(valores[14]));
+            engineScope.put("P4_CLOSE" ,Double.parseDouble(valores[15]));
             
-            engineScope.put("P5_OPEN" ,Double.parseDouble(arq.lerArquivo("Open", tam+4)));
-            engineScope.put("P5_MAX" ,Double.parseDouble(arq.lerArquivo("High", tam+4)));
-            engineScope.put("P5_MIN" ,Double.parseDouble(arq.lerArquivo("Low", tam+4)));
-            engineScope.put("P5_CLOSE" ,Double.parseDouble(arq.lerArquivo("Close", tam+4)));
+            engineScope.put("P5_OPEN" ,Double.parseDouble(valores[16]));
+            engineScope.put("P5_MAX" ,Double.parseDouble(valores[17]));
+            engineScope.put("P5_MIN" ,Double.parseDouble(valores[18]));
+            engineScope.put("P5_CLOSE" ,Double.parseDouble(valores[19]));
             
             
             if(engine.eval(exp, newContext).toString().equals("true")){ 
-            	System.out.println("true");
             	counter++;
-            	engineScope.put("P1_CLOSE" ,Double.parseDouble(arq.lerArquivo("Close", tam)));
-            	engineScope.put("P2_CLOSE" ,Double.parseDouble(arq.lerArquivo("Close", tam-1)));
-            	engineScope.put("P3_CLOSE" ,Double.parseDouble(arq.lerArquivo("Close", tam-2)));
-            	engineScope.put("P4_CLOSE" ,Double.parseDouble(arq.lerArquivo("Close", tam-3)));
+            	engineScope.put("P1_CLOSE" ,Double.parseDouble(valores[20]));
+            	engineScope.put("P2_CLOSE" ,Double.parseDouble(valores[21]));
+            	engineScope.put("P3_CLOSE" ,Double.parseDouble(valores[22]));
+            	engineScope.put("P4_CLOSE" ,Double.parseDouble(valores[23]));
             	if(engine.eval("P1_CLOSE * 3 <= (P2_CLOSE + P3_CLOSE + P4_CLOSE)", newContext).toString().equals("true")){
             		acertos++;
             	}
             } else {
-            	System.out.println("false");
+            	
             }
         }
-        String result = counter + " vezes encontradas e " + acertos + " acertos na simulação de compra.";
+        String result = counter + " vezes encontradas e " + acertos + " acertos na simulaï¿½ï¿½o de compra.";
         
         return result;
 	}
--- BolsaEscravo/src/LeituraArquivo.java
+++ BolsaEscravo/src/LeituraArquivo.java
@@ -31,7 +31,7 @@ public class LeituraArquivo {
 		return records.size();
 	}
 	
-	public String lerArquivo(String valor, int pos) {
+	public String[] lerArquivo(int pos) {
 		// TODO Auto-generated method stub
 		List<CSVRecord> records;
 		try {
@@ -43,9 +43,37 @@ public class LeituraArquivo {
 			
 			
 			int n = records.size();
-			String retorno;
+			String retorno[] = new String[24];
 			if(pos <= n) {			
-				retorno = records.get(pos).get(valor);
+				retorno[0] = records.get(pos).get("Open");
+				retorno[1] = records.get(pos).get("High");
+				retorno[2] = records.get(pos).get("Low");
+				retorno[3] = records.get(pos).get("Close");
+				
+				retorno[4] = records.get(pos+1).get("Open");
+				retorno[5] = records.get(pos+1).get("High");
+				retorno[6] = records.get(pos+1).get("Low");
+				retorno[7] = records.get(pos+1).get("Close");
+				
+				retorno[8] = records.get(pos+2).get("Open");
+				retorno[9] = records.get(pos+2).get("High");
+				retorno[10] = records.get(pos+2).get("Low");
+				retorno[11] = records.get(pos+2).get("Close");
+				
+				retorno[12] = records.get(pos+3).get("Open");
+				retorno[13] = records.get(pos+3).get("High");
+				retorno[14] = records.get(pos+3).get("Low");
+				retorno[15] = records.get(pos+3).get("Close");
+				
+				retorno[16] = records.get(pos+4).get("Open");
+				retorno[17] = records.get(pos+4).get("High");
+				retorno[18] = records.get(pos+4).get("Low");
+				retorno[19] = records.get(pos+4).get("Close");
+				
+				retorno[20] = records.get(pos).get("Close");
+				retorno[21] = records.get(pos-1).get("Close");
+				retorno[22] = records.get(pos-2).get("Close");
+				retorno[23] = records.get(pos-3).get("Close");
 			} else retorno = null;
 			
 			return retorno;
